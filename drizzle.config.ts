4cec3b0a24abb626af01cd9ad6bec49c489c89fe
Mac Pro <macpro@MacBook-Pro-de-Mac.local>
2026-05-10 22:28:36 -0300

    fix: use modern drizzle-kit push command

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export default {
   schema: './src/server/db/schema.ts',
   out: './drizzle',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL || '',
   },
